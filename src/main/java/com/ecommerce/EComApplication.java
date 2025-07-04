74118c777155c2d9b1b4f55e5f1e917f8f7c01aa
RaveendarAnburose <raveendaranburose20@gmail.com>
2025-07-04 10:22:02 +0530

    Add files via upload

---
+++ src/main/java/com/ecommerce/EComApplication.java
@@ -0,0 +1,11 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EComApplication.class, args);
+    }
+}
